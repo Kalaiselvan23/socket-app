7b3ec246f47bb11e697cb61fe8ae0f42797bd17b
Kalaiselvan23 <kalaiselvan1623@gmail.com>
2024-02-21 22:45:25 +0530

    messages are pushed into db

--- socket-chat-app/src/components/components/ChatBubble.tsx
+++ socket-chat-app/src/components/components/ChatBubble.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { UserContextType } from '@/types'
+import React, { useContext } from 'react'
+import { UserContext } from '../../contexts/UserContext';
+type messageType={
+  senderId:string,
+  chatId:string,
+  text:string,
+  createdAt:string,
+}
 
-const ChatBubble = ({message}:{message:string}) => {
+const ChatBubble = ({message}:{message:messageType}) => {
+  const {User}=useContext<UserContextType>(UserContext);
+  console.log(typeof message.createdAt)
   return (
-    <div className='bg-gray-200 p-2 chat-bubble'>{message}</div>
+    <div className={`flex my-4 justify-start ${message.senderId==User.userId ? "justify-end":""}`}>
+      <div>
+      <div className='bg-gray-200 p-2 chat-bubble'>{message.text}</div>
+      <span>{message.createdAt}</span>
+      </div>
+    </div>
   )
 }
 
--- socket-chat-app/src/components/components/ChatMessages.tsx
+++ socket-chat-app/src/components/components/ChatMessages.tsx
@@ -1,7 +1,11 @@
 import { useSelectedChat } from "@/contexts/SelectedChatContext"
+import { useQueryClient } from "react-query";
 
 const ChatMessages = ({chatName,chatId}:{chatName:string,chatId:string}) => {
   const {chat,setChat}=useSelectedChat();
+  const data=useQueryClient().getQueryData([`textmessages`])
+  const lastData= data[data?.length-1];
+  console.log(lastData?.text);
   const handleClick=()=>{
     setChat({chatId,chatName})
     console.log(chatId)
@@ -11,7 +15,7 @@ const ChatMessages = ({chatName,chatId}:{chatName:string,chatId:string}) => {
         <h4>{chatName}</h4>
         <div className="flex">
             <span className="font-semibold">Guest User:</span>
-            <p>Kalaiselvan hii!!</p>
+            <p>{lastData?.text}</p>
         </div>
     </div>
   )
